ae300bb7e97e247b450aea945066ee712d893f03
ZyryanovAV <ar200103zeranov@yandex.ru>
2020-12-07 04:24:05 +0300

    Add all file

---
+++ Individual1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Создайте рекурсивную функцию, печатающую все возможные перестановки для целых
+# чисел от 1 до N.
+
+import sys
+
+
+def permutation(s):
+    if len(s) == 1:
+        return [s]
+
+    perm_list = []
+    for a in s:
+        elements = [x for x in s if x != a]
+        z = permutation(elements)
+
+        for t in z:
+            perm_list.append([a] + t)
+
+    return perm_list
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(range(1, n + 1))
+
+    for line in permutation(arr):
+        print(line)
